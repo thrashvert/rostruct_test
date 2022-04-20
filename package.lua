6b2ea36ca3bd92e6056694ebb5ca8fd887380d24
thrashvert <75057533+thrashvert@users.noreply.github.com>
2022-04-20 14:43:49 -0400

    Update package.lua

--- package.lua
+++ package.lua
@@ -1,1 +1,9 @@
-return print("Hello World!")
+local Rostruct = loadstring(game:HttpGetAsync("https://github.com/richie0866/Rostruct/releases/download/v1.1.10-beta/Rostruct.lua"))()
+
+-- Download the latest release to local files
+Rostruct.fetchLatest("thrashvert", "rostruct_test")
+-- Then, build and start all scripts
+:andThen(function(package)
+    package:build("src/")
+    package:start()
+end)
